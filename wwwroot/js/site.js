0e74a805b3904ff8b0ae905985ad989b0f631ad3
Treewit <98694993+FiFa254@users.noreply.github.com>
2026-04-10 13:12:08 +0700

    Add project files.

---
+++ wwwroot/js/site.js
@@ -0,0 +1,5 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("toggleSidebar").addEventListener("click", function () {
+        document.getElementById("sidebar").classList.toggle("collapsed");
+    });
+});
